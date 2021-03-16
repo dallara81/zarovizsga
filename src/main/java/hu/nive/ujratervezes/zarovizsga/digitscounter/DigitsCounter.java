7d85077609d72915625dd27c0bd1dba956afcd88
dallara81 <pluggasvenska@yahoo.se>
2021-03-16 21:57:24 +0100

    Záróvizsga feladat - DigitsCounter

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -1,4 +1,23 @@
 package hu.nive.ujratervezes.zarovizsga.digitscounter;
 
+import java.util.TreeSet;
+import java.util.Set;
+
 public class DigitsCounter {
+
+    public int getCountOfDigits(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+
+        Set<Character> digits = new TreeSet<>();
+
+        for (char c: s.toCharArray()) {
+            if (c >= '0' && c <= '9') {
+                digits.add(c);
+            }
+        }
+
+        return digits.size();
+    }
 }
